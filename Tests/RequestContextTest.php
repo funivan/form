71a3d5d1756ae8e394c98a431af8ca0e282847f5
vitaliy <vitaliy.s@autoline.com.ua>
2016-05-24 10:28:29 +0300

    #25 created RequestContext class + test

---
+++ Tests/RequestContextTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+  namespace Tests\Fiv\Form;
+
+
+  use Fiv\Form\RequestContext;
+
+  class RequestContextTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidHttpMethodValue() {
+      new RequestContext('invalid', []);
+    }
+
+
+    public function testRequestContext() {
+      $testData = [
+        'name' => 'petro',
+        'email' => 'petro@gmail.com',
+      ];
+      $requestContext = new RequestContext(RequestContext::METHOD_POST, $testData);
+
+      $this->assertEquals(RequestContext::METHOD_POST, $requestContext->getMethod());
+      $this->assertFalse($requestContext->isMethod(RequestContext::METHOD_GET));
+      $this->assertTrue($requestContext->isMethod(RequestContext::METHOD_POST));
+
+      $this->assertEquals(['name' => 'petro', 'email' => 'petro@gmail.com'], $requestContext->all());
+
+      $this->assertEquals('petro', $requestContext->get('name'));
+      $this->assertNull($requestContext->get('age'));
+
+      $this->assertTrue($requestContext->has('email'));
+      $this->assertFalse($requestContext->has('age'));
+    }
+  }
---
+++ src/RequestContext.php
@@ -0,0 +1,83 @@
+<?php
+
+
+  namespace Fiv\Form;
+
+
+  /**
+   *
+   */
+  class RequestContext {
+
+    CONST METHOD_GET = 'GET';
+
+    CONST METHOD_POST = 'POST';
+
+    /**
+     * @var string
+     */
+    protected $method = null;
+
+    /**
+     * @var array
+     */
+    protected $attributes = [];
+
+
+    /**
+     * @param string $method
+     * @param array $attributes
+     */
+    public function __construct($method, array $attributes = []) {
+      if (!is_string($method)) {
+        throw new \InvalidArgumentException('Parameter "method" should be a string,  ' . gettype($method) . ' given.');
+      }
+      $method = mb_strtoupper($method);
+      if (!in_array($method, [self::METHOD_GET, self::METHOD_POST])) {
+        throw new \InvalidArgumentException('Invalid http method name.');
+      }
+      $this->method = $method;
+      $this->attributes = $attributes;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getMethod() {
+      return $this->method;
+    }
+
+
+    public function isMethod($method) {
+      return $this->method == $method;
+    }
+
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function has($name) {
+      return isset($this->attributes[$name]);
+    }
+
+
+    /**
+     * @param string $name
+     * @return mixed|null
+     */
+    public function get($name) {
+      return isset($this->attributes[$name]) ? $this->attributes[$name] : null;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function all() {
+      return $this->attributes;
+    }
+
+
+  }
